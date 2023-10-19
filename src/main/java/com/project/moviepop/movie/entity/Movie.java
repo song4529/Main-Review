249c83a157a165bcbd885cdcffd97f1bc396cff3
Song <gusdn4529@gmail.com>
2023-10-19 16:01:21 +0900

    feat : Add the required field

--- src/main/java/com/project/moviepop/movie/entity/Movie.java
+++ src/main/java/com/project/moviepop/movie/entity/Movie.java
@@ -1,4 +1,11 @@
 package com.project.moviepop.movie.entity;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Movie {
+    private String movieCode;
+    private String title;
+    private String genre;
+    private String isAdulted;
 }
