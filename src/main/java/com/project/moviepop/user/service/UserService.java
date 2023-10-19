fe6ad8eed12ee63fb8f9e96d7447ea9489f20f8f
Song <gusdn4529@gmail.com>
2023-10-19 16:03:27 +0900

    feat : Add the required annotation

--- src/main/java/com/project/moviepop/user/service/UserService.java
+++ src/main/java/com/project/moviepop/user/service/UserService.java
@@ -1,4 +1,10 @@
 package com.project.moviepop.user.service;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
 public class UserService {
 }
