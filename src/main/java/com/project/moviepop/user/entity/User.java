f311973f6c33f6b1573963e5aeef79ff0f371976
Song <gusdn4529@gmail.com>
2023-10-19 16:02:02 +0900

    feat : Add the required field

--- src/main/java/com/project/moviepop/user/entity/User.java
+++ src/main/java/com/project/moviepop/user/entity/User.java
@@ -1,4 +1,41 @@
 package com.project.moviepop.user.entity;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = false, length = 100)
+    private String nickname;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    private String profileImage;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String birth;
+
+    private float star = 0;
+
+    private UserStatus userStatus = UserStatus.USER_ACTIVE;
+
+    //매핑 필요한 것들 보류
 }
