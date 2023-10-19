35c4a0b9e7615588e5b41aecc2332b0b3735f84a
Song <gusdn4529@gmail.com>
2023-10-19 16:02:47 +0900

    feat : Add the required method

--- src/main/java/com/project/moviepop/user/dto/UserDto.java
+++ src/main/java/com/project/moviepop/user/dto/UserDto.java
@@ -1,4 +1,32 @@
 package com.project.moviepop.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class UserDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Post{
+        @NotBlank
+        @Email
+        private String email;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch{
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class PatchPassword{
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response{
+    }
 }
