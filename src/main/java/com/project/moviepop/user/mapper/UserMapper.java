38caf1128cadb5fd75380d427c7d6cc88662c049
Song <gusdn4529@gmail.com>
2023-10-20 15:21:40 +0900

    feat : Mapping UserPostDto to User

--- src/main/java/com/project/moviepop/user/mapper/UserMapper.java
+++ src/main/java/com/project/moviepop/user/mapper/UserMapper.java
@@ -1,4 +1,8 @@
 package com.project.moviepop.user.mapper;
 
+import com.project.moviepop.user.dto.UserDto;
+import com.project.moviepop.user.entity.User;
+
 public interface UserMapper {
+    User userPostDtoToUser(UserDto.Post userPostDto);
 }
