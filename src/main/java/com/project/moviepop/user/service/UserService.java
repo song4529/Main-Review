f3113fd818b4db29331240fdbe582dd052e8fffd
Song <gusdn4529@gmail.com>
2023-10-20 15:22:30 +0900

    feat : Add the necessary features to sign up

--- src/main/java/com/project/moviepop/user/service/UserService.java
+++ src/main/java/com/project/moviepop/user/service/UserService.java
@@ -1,10 +1,30 @@
 package com.project.moviepop.user.service;
 
+import com.project.moviepop.exception.BusinessLogicException;
+import com.project.moviepop.exception.ExceptionCode;
+import com.project.moviepop.user.entity.User;
+import com.project.moviepop.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Transactional
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User creatUser(User user) {
+        User findUser = userRepository.findByEmail(user.getEmail());
+        checkEmailExist(findUser);
+        return userRepository.save(user);
+    }
+
+    public boolean checkEmailExist(User user) {
+        if(userRepository.findByEmail(user.getEmail()) != null) {
+            throw new BusinessLogicException(ExceptionCode.USER_EXISTS);
+        }
+        return true;
+    }
 }
