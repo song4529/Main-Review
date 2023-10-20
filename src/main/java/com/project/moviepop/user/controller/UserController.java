29df4b5d1a69775fa88ac84bb463c17460ffb248
Song <gusdn4529@gmail.com>
2023-10-20 15:20:01 +0900

    feat : Add membership function

--- src/main/java/com/project/moviepop/user/controller/UserController.java
+++ src/main/java/com/project/moviepop/user/controller/UserController.java
@@ -2,6 +2,10 @@ package com.project.moviepop.user.controller;
 
 
 import com.project.moviepop.user.dto.UserDto;
+import com.project.moviepop.user.entity.User;
+import com.project.moviepop.user.mapper.UserMapper;
+import com.project.moviepop.user.service.UserService;
+import com.project.moviepop.utils.UriComponent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
@@ -18,8 +23,21 @@ import javax.validation.constraints.Positive;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final static String  USER_DEFAULT_URI = "/users";
+    private final UserMapper userMapper;
+    private final UserService userService;
+
     @PostMapping //회원가입
     public ResponseEntity postUser(@Valid @RequestBody UserDto.Post userPostDto) {
+        //User를 userPostDto에서 받아온 정보를 통해서 user를 생성한다
+        User user = userMapper.userPostDtoToUser(userPostDto);
+
+        //user를 만드는 서비스가 필요하다
+        User createUser = userService.creatUser(user);
+
+        //사용자를 나타내는 URI를 생성
+        URI uri = UriComponent.createUri(USER_DEFAULT_URI, createUser.getUserId());
+
         return null;
     }
 
