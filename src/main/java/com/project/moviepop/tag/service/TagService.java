2c963e8a15a3bc4a0fdf64bf7a8d10543a0ea351
Song <gusdn4529@gmail.com>
2023-10-21 16:07:29 +0900

    feat : TagService 기본 틀 작업

--- src/main/java/com/project/moviepop/tag/service/TagService.java
+++ src/main/java/com/project/moviepop/tag/service/TagService.java
@@ -1,4 +1,16 @@
 package com.project.moviepop.tag.service;
 
+import com.project.moviepop.tag.repository.TagRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class TagService {
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
 }
