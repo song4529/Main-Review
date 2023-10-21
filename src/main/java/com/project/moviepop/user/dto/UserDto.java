87c2a95c26d16b38bbe14d1f1a06626254102629
Song <gusdn4529@gmail.com>
2023-10-21 15:59:44 +0900

    fix : Dto로 사용될 필드 수정

--- src/main/java/com/project/moviepop/user/dto/UserDto.java
+++ src/main/java/com/project/moviepop/user/dto/UserDto.java
@@ -1,10 +1,14 @@
 package com.project.moviepop.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 public class UserDto {
     @Getter
@@ -13,20 +17,64 @@ public class UserDto {
         @NotBlank
         @Email
         private String email;
+
+        @NotBlank
+        private String nickname;
+
+        private String profileImage;
+
+        @NotBlank
+        private String name;
+
+        @NotBlank
+        private String birth;
+
+        @NotBlank
+        private String password;
+
+        /*@NotNull
+        private List<TagDto.UserRequest> tags;*/
     }
 
     @Getter
     @AllArgsConstructor
     public static class Patch{
+        @Setter
+        private long userId;
+        @NotBlank
+        private String nickname;
+        private String profileImage;
+
+        /*@NotNull
+        private List<TagDto.UserRequest> tags;*/
     }
 
     @Getter
     @AllArgsConstructor
     public static class PatchPassword{
+        @NotBlank
+        private String currentPassword;
+
+        @NotBlank
+        private String newPassword;
+    }
+
+    @Getter
+    @Builder
+    public static class Response{
+        private Long userId;
+        private String name;
+        private String nickname;
+        private String email;
+        private String profileImage;
+//        private List<TagDto.UserRequest> tags;
     }
 
     @Getter
     @AllArgsConstructor
-    public static class Response{
+    public static class ReviewBoardResponse {
+        private long userId;
+        private String username;
+        private String profileImage;
     }
 }
