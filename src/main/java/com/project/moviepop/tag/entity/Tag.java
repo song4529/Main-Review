791207ccdd9689f0b5225360dea363fff8fee4e6
Song <gusdn4529@gmail.com>
2023-10-21 16:06:40 +0900

    feat : tag 엔티티 기초작업 추가

--- src/main/java/com/project/moviepop/tag/entity/Tag.java
+++ src/main/java/com/project/moviepop/tag/entity/Tag.java
@@ -1,4 +1,22 @@
 package com.project.moviepop.tag.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tagId;
+
+    private String tagName;
 }
