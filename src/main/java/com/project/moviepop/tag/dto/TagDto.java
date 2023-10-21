6e01b3cfc22b9f0b2fa9e777b4c19f0d90415592
Song <gusdn4529@gmail.com>
2023-10-21 16:06:58 +0900

    feat : tag관련 사용될 Dto 추가

--- src/main/java/com/project/moviepop/tag/dto/TagDto.java
+++ src/main/java/com/project/moviepop/tag/dto/TagDto.java
@@ -1,4 +1,22 @@
 package com.project.moviepop.tag.dto;
 
+import lombok.Getter;
+
 public class TagDto {
+
+    @Getter
+    public static class response {
+        private long tagId;
+        private String tagName;
+    }
+
+    @Getter
+    public static class ReviewBoardRequest {
+        private long tagId;
+    }
+
+    @Getter
+    public static class UserRequest {
+        private long tagId;
+    }
 }
