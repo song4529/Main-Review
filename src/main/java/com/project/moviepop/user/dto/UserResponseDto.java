80c1792cd03a749867769abd03d37613f463a637
Song <gusdn4529@gmail.com>
2023-10-22 15:01:38 +0900

    feat : 개별적인 UserResponseDto 추가

---
+++ src/main/java/com/project/moviepop/user/dto/UserResponseDto.java
@@ -0,0 +1,10 @@
+package com.project.moviepop.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserResponseDto {
+    private UserDto.Response user;
+}
