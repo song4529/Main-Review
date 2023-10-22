f8e63969d87a5176f7042854a0c4cfe59b087799
Song <gusdn4529@gmail.com>
2023-10-22 15:01:19 +0900

    feat : userToUserPatchDto 및 userToUserResponseDto 매핑작업 추가

--- src/main/java/com/project/moviepop/user/mapper/UserMapper.java
+++ src/main/java/com/project/moviepop/user/mapper/UserMapper.java
@@ -1,10 +1,15 @@
 package com.project.moviepop.user.mapper;
 
 import com.project.moviepop.user.dto.UserDto;
+import com.project.moviepop.user.dto.UserResponseDto;
 import com.project.moviepop.user.entity.User;
 
 public interface UserMapper {
     User userPostDtoToUser(UserDto.Post userPostDto);
 
     User userPatchDtoToUser(UserDto.Patch userPatchDto);
+
+    UserDto.Patch userToUserPatchDto(User user);
+
+    UserResponseDto userToUserResponseDto(User user);
 }
