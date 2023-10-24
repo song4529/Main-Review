900142e449409d7b3b795bc4fb53848fd1622e01
Song <gusdn4529@gmail.com>
2023-10-24 15:37:03 +0900

    feat : 리뷰보드 수정을 하기 위해 필요한 매핑 작업 추가

--- src/main/java/com/project/moviepop/reviewboard/mapper/ReviewBoardMapper.java
+++ src/main/java/com/project/moviepop/reviewboard/mapper/ReviewBoardMapper.java
@@ -2,10 +2,32 @@ package com.project.moviepop.reviewboard.mapper;
 
 import com.project.moviepop.reviewboard.dto.ReviewBoardDto;
 import com.project.moviepop.reviewboard.entity.ReviewBoard;
+import com.project.moviepop.user.dto.UserDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = "spring")
 public interface ReviewBoardMapper {
     ReviewBoard PostToReviewBoard(ReviewBoardDto.Post post);
+    ReviewBoard PatchToReviewBoard(ReviewBoardDto.Patch patch);
+
+    default ReviewBoardDto.Response reviewBoardToResponse(ReviewBoard reviewBoard) {
+        UserDto.ReviewBoardResponse userResponse = new UserDto.ReviewBoardResponse(
+                reviewBoard.getUser().getUserId(),
+                reviewBoard.getUser().getNickname(),
+                reviewBoard.getUser().getProfileImage()
+        );
+
+        ReviewBoardDto.Response response = ReviewBoardDto.Response.builder()
+                .reviewBoardId(reviewBoard.getReviewBoardId())
+                .title(reviewBoard.getTitle())
+                .review(reviewBoard.getReview())
+                .wish(reviewBoard.getWish())
+                .thumbnail(reviewBoard.getThumbnail())
+                .createdAt(reviewBoard.getCreatedAt())
+                .user(userResponse)
+                .build();
+
+        return response;
+    }
 }
