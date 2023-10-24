847107112021fc9bb73629bd931df435cb75098b
Song <gusdn4529@gmail.com>
2023-10-24 15:37:25 +0900

    feat : 리뷰보드 수정을 하기 위해 필요한 서비스 메서드 추가

--- src/main/java/com/project/moviepop/reviewboard/service/ReviewBoardService.java
+++ src/main/java/com/project/moviepop/reviewboard/service/ReviewBoardService.java
@@ -1,5 +1,7 @@
 package com.project.moviepop.reviewboard.service;
 
+import com.project.moviepop.exception.BusinessLogicException;
+import com.project.moviepop.exception.ExceptionCode;
 import com.project.moviepop.reviewboard.dto.ReviewBoardDto;
 import com.project.moviepop.reviewboard.entity.ReviewBoard;
 import com.project.moviepop.reviewboard.mapper.ReviewBoardMapper;
@@ -7,8 +9,10 @@ import com.project.moviepop.reviewboard.repository.ReviewBoardRepository;
 import com.project.moviepop.user.entity.User;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -20,5 +24,27 @@ public class ReviewBoardService {
     public ReviewBoard createReviewBoard(ReviewBoardDto.Post post) {
         ReviewBoard newReviewBoard = reviewBoardMapper.PostToReviewBoard(post);
         return newReviewBoard;
+    }
+
+    public ReviewBoard updateReviewBoard(long userId, ReviewBoard reviewBoard) {
+        ReviewBoard getReviewBoard = findReviewBoard(reviewBoard.getReviewBoardId());
+        if(getReviewBoard.getUser().getUserId() != userId)
+            throw new BusinessLogicException(ExceptionCode.INVALID_USER);
+
+        Optional.ofNullable(reviewBoard.getTitle())
+                .ifPresent(title -> getReviewBoard.setTitle(title));
+        Optional.ofNullable(reviewBoard.getReview())
+                .ifPresent(review -> getReviewBoard.setReview(review));
+
+        return reviewBoardRepository.save(getReviewBoard);
+    }
+
+    //추가적인 매서드
+    @Transactional(readOnly = true)
+    public ReviewBoard findReviewBoard(long reviewBoardId) {
+        Optional<ReviewBoard> optionalReviewBoard = reviewBoardRepository.findById(reviewBoardId);
+        ReviewBoard findReviewBoard =
+                optionalReviewBoard.orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVIEW_BOARD_NOT_FOUND));
+        return findReviewBoard;
     }
 }
