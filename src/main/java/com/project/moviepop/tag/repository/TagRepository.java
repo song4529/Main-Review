75812787ae85ad90d93869dbb4bd5049359caa24
Song <gusdn4529@gmail.com>
2023-10-25 23:45:44 +0900

    feat : TagRepository에 JpaRepository 의존성 추가

--- src/main/java/com/project/moviepop/tag/repository/TagRepository.java
+++ src/main/java/com/project/moviepop/tag/repository/TagRepository.java
@@ -1,4 +1,7 @@
 package com.project.moviepop.tag.repository;
 
-public interface TagRepository {
+import com.project.moviepop.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
 }
