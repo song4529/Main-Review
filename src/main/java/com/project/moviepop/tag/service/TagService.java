f66f128a5e8f155e289d69c47d2bb87d0dde6422
Song <gusdn4529@gmail.com>
2023-10-25 23:46:09 +0900

    feat : Tag를 TagRepository를 통해 가져오는 메서드 추가

--- src/main/java/com/project/moviepop/tag/service/TagService.java
+++ src/main/java/com/project/moviepop/tag/service/TagService.java
@@ -1,9 +1,11 @@
 package com.project.moviepop.tag.service;
 
+import com.project.moviepop.tag.entity.Tag;
 import com.project.moviepop.tag.repository.TagRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -12,5 +14,9 @@ public class TagService {
 
     public TagService(TagRepository tagRepository) {
         this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> getTags() {
+        return tagRepository.findAll();
     }
 }
