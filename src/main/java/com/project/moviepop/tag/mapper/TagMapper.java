2654856658ddd8192d97b0bcb97d2cca7ef94c38
Song <gusdn4529@gmail.com>
2023-10-25 23:45:17 +0900

    feat : Tag와 Response를 매핑해주는 메서드 구현

--- src/main/java/com/project/moviepop/tag/mapper/TagMapper.java
+++ src/main/java/com/project/moviepop/tag/mapper/TagMapper.java
@@ -1,4 +1,12 @@
 package com.project.moviepop.tag.mapper;
 
+import com.project.moviepop.tag.dto.TagDto;
+import com.project.moviepop.tag.entity.Tag;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface TagMapper {
+    List<TagDto.response> tagsToResponse(List<Tag> tags);
 }
