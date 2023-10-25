c877c3e39527bb3044316b65b636f9ebe978f0ec
Song <gusdn4529@gmail.com>
2023-10-25 23:44:59 +0900

    feat : 태그를 가져오는 getAllTags 메서드 구현

--- src/main/java/com/project/moviepop/tag/controller/TagController.java
+++ src/main/java/com/project/moviepop/tag/controller/TagController.java
@@ -1,4 +1,26 @@
 package com.project.moviepop.tag.controller;
 
+import com.project.moviepop.dto.ResponseDto;
+import com.project.moviepop.tag.mapper.TagMapper;
+import com.project.moviepop.tag.service.TagService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tags")
+@Validated
+@AllArgsConstructor
 public class TagController {
+    private final TagService tagService;
+    private final TagMapper tagMapper;
+
+    @GetMapping
+    public ResponseEntity getAllTags() {
+        return new ResponseEntity<>(new ResponseDto.SingleResponseDto(tagMapper.tagsToResponse(tagService.getTags())), HttpStatus.OK);
+    }
 }
