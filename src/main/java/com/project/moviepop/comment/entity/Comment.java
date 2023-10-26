029c9225af18fd47fbd329a4783b84927ee8892e
Song <gusdn4529@gmail.com>
2023-10-26 14:36:28 +0900

    feat : Comment 엔티티의 의존성 추가 및 필드 추가, ReviewBoard와의 매핑 추가

--- src/main/java/com/project/moviepop/comment/entity/Comment.java
+++ src/main/java/com/project/moviepop/comment/entity/Comment.java
@@ -1,4 +1,52 @@
 package com.project.moviepop.comment.entity;
 
-public class Comment {
+import com.project.moviepop.audit.Auditable;
+import com.project.moviepop.reviewboard.entity.ReviewBoard;
+import com.project.moviepop.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Comment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+    @Column(nullable = false, length = 2000)
+    private String content;
+    @Column(nullable = false)
+    private Integer likes = 0;
+
+    @ManyToOne
+    @JoinColumn(name = "REVIEW_BOARD_ID")
+    private ReviewBoard reviewBoard;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public void setReviewBoard(ReviewBoard reviewBoard) {
+        this.reviewBoard = reviewBoard;
+        if(!reviewBoard.getComments().contains(this))
+            reviewBoard.getComments().add(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(commentId, comment.commentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(commentId);
+    }
 }
