5f3894688d9c7837b3d64bec19e25ca50df93f30
Song <gusdn4529@gmail.com>
2023-10-26 14:38:48 +0900

    feat : CommentService 기본 애너테이션 작업 및 postComment에 필요한 서비스인 createComment 메서드 추가

--- src/main/java/com/project/moviepop/comment/service/CommentService.java
+++ src/main/java/com/project/moviepop/comment/service/CommentService.java
@@ -1,4 +1,25 @@
 package com.project.moviepop.comment.service;
 
+import com.project.moviepop.comment.entity.Comment;
+import com.project.moviepop.comment.repository.CommentRepository;
+import com.project.moviepop.reviewboard.entity.ReviewBoard;
+import com.project.moviepop.reviewboard.service.ReviewBoardService;
+import com.project.moviepop.user.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@AllArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public Comment createComment(ReviewBoard reviewBoard, User user, Comment comment) {
+        comment.setUser(user);
+        comment.setReviewBoard(reviewBoard);
+
+        return commentRepository.save(comment);
+    }
 }
