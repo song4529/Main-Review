33bdd6fad4015b01833c73c0bdbead3900f63991
Song <gusdn4529@gmail.com>
2023-10-26 14:37:46 +0900

    feat : commentPostDtoToComment 매핑 작업 추가

--- src/main/java/com/project/moviepop/comment/mapper/CommentMapper.java
+++ src/main/java/com/project/moviepop/comment/mapper/CommentMapper.java
@@ -1,4 +1,10 @@
 package com.project.moviepop.comment.mapper;
 
+import com.project.moviepop.comment.dto.CommentDto;
+import com.project.moviepop.comment.entity.Comment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CommentMapper {
+    Comment commentPostDtoToComment(CommentDto.Post postDto);
 }
