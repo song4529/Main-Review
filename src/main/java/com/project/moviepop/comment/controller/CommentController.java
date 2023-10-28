2f6acf27c83b7b0c287860a20648f7698ba5290e
Song <gusdn4529@gmail.com>
2023-10-28 20:05:30 +0900

    feat : patchComment 메서드 구현

--- src/main/java/com/project/moviepop/comment/controller/CommentController.java
+++ src/main/java/com/project/moviepop/comment/controller/CommentController.java
@@ -4,12 +4,14 @@ import com.project.moviepop.comment.dto.CommentDto;
 import com.project.moviepop.comment.entity.Comment;
 import com.project.moviepop.comment.mapper.CommentMapper;
 import com.project.moviepop.comment.service.CommentService;
+import com.project.moviepop.dto.ResponseDto;
 import com.project.moviepop.reviewboard.entity.ReviewBoard;
 import com.project.moviepop.reviewboard.service.ReviewBoardService;
 import com.project.moviepop.user.entity.User;
 import com.project.moviepop.user.service.UserService;
 import com.project.moviepop.utils.UriComponent;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -41,5 +43,18 @@ public class CommentController {
         URI location = UriComponent.createUri(COMMENT_DEFAULT_URL, comment.getCommentId());
 
         return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/{comment-id}/users/{user-id}")
+    public ResponseEntity patchComment(@PathVariable("comment-id") @Positive long commentId,
+                                       @PathVariable("user-id") @Positive long userId,
+                                       @RequestBody @Valid CommentDto.Patch requestBody) {
+        requestBody.setCommentId(commentId);
+        Comment comment = commentService.updateComment(userId, mapper.commentPatchDtoToComment(requestBody));
+
+        return new ResponseEntity (
+                new ResponseDto.SingleResponseDto(commentMapper.commentToCommentPatchResponseDto(comment)),
+                HttpStatus.OK
+        );
     }
 }
