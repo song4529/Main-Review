2eb2fba819463f52d5f7ab615e84e42034b3f961
Song <gusdn4529@gmail.com>
2023-10-29 17:44:55 +0900

    feat : ResponseDto 수정

--- src/main/java/com/project/moviepop/dto/ResponseDto.java
+++ src/main/java/com/project/moviepop/dto/ResponseDto.java
@@ -2,6 +2,7 @@ package com.project.moviepop.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -17,9 +18,9 @@ public class ResponseDto {
         private List<T> data;
         private PageInfo pageInfo;
 
-        public MultipleResponseDto(List<T> data, PageInfo pageInfo) {
+        public MultipleResponseDto(List<T> data, Page page) {
             this.data = data;
-            this.pageInfo = pageInfo;
+            this.pageInfo = new PageInfo(page.getNumber(), page.getSize(), page.getTotalElements(), page.getTotalPages());
         }
     }
 }
