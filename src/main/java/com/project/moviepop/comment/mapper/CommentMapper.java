047bc4f2d82e9e5605683167360a7677d176c649
Song <gusdn4529@gmail.com>
2023-10-29 17:43:43 +0900

    feat : CommentController에서 원하는 값을 매핑해준 다음 리턴하기 위한 매핑 작업

--- src/main/java/com/project/moviepop/comment/mapper/CommentMapper.java
+++ src/main/java/com/project/moviepop/comment/mapper/CommentMapper.java
@@ -4,7 +4,14 @@ import com.project.moviepop.comment.dto.CommentDto;
 import com.project.moviepop.comment.entity.Comment;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
     Comment commentPostDtoToComment(CommentDto.Post postDto);
+    Comment commentPatchDtoToComment(CommentDto.Patch patchDto);
+    CommentDto.PatchResponse commentToCommentPatchResponseDto(Comment comment);
+    CommentDto.Response commentToCommentResponseDto(Comment comment);
+    List<CommentDto.Response> commentsToCommentResponseDtos(List<Comment> comments);
+    CommentDto.likeResponse commentToCommentLikeResponse(Comment comment);
 }
