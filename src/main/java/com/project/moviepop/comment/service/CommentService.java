1ef56cea9b83e1cac4f2e97d4514fa6100234a0c
Song <gusdn4529@gmail.com>
2023-10-29 17:44:24 +0900

    feat : comment를 update, find 하는 메서드 구현

--- src/main/java/com/project/moviepop/comment/service/CommentService.java
+++ src/main/java/com/project/moviepop/comment/service/CommentService.java
@@ -2,13 +2,19 @@ package com.project.moviepop.comment.service;
 
 import com.project.moviepop.comment.entity.Comment;
 import com.project.moviepop.comment.repository.CommentRepository;
+import com.project.moviepop.exception.BusinessLogicException;
+import com.project.moviepop.exception.ExceptionCode;
 import com.project.moviepop.reviewboard.entity.ReviewBoard;
 import com.project.moviepop.reviewboard.service.ReviewBoardService;
 import com.project.moviepop.user.entity.User;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -23,5 +29,30 @@ public class CommentService {
         comment.setReviewBoard(reviewBoard);
 
         return commentRepository.save(comment);
+    }
+
+    public Comment updateComment(long userId, Comment comment) {
+        Comment findComment = findComment(comment.getCommentId());
+        if(findComment.getUser().getUserId() != userId)
+            throw new BusinessLogicException(ExceptionCode.CANNOT_UPDATE_COMMENT);
+
+        Optional.ofNullable(comment.getContent())
+                .ifPresent(content -> findComment.setContent(content));
+
+        return commentRepository.save(findComment);
+    }
+
+
+    public Comment findComment(long commentId) {
+        Optional<Comment> optionalComment = commentRepository.findById(commentId);
+        Comment comment = optionalComment.orElseThrow(() -> new BusinessLogicException(ExceptionCode.COMMENT_NOT_FOUND));
+
+        return comment;
+    }
+
+    public Page<Comment> findComments(int page, int size) {
+        return commentRepository.findAll(PageRequest.of(
+                page - 1, size, Sort.by("commentId").descending())
+        );
     }
 }
