5f4dd93ad83ff2776e3ee91f96c30752cb7ac13f
Song <gusdn4529@gmail.com>
2023-11-01 15:43:26 +0900

    fix : Movie 필드 보완

--- src/main/java/com/project/moviepop/movie/entity/Movie.java
+++ src/main/java/com/project/moviepop/movie/entity/Movie.java
@@ -2,10 +2,35 @@ package com.project.moviepop.movie.entity;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 public class Movie {
     private String movieCode;
     private String title;
     private String genre;
     private String isAdulted;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return movieCode == movie.movieCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(movieCode);
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieCode='" + movieCode + '\'' +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", isAdulted='" + isAdulted + '\'' +
+                '}';
+    }
 }
