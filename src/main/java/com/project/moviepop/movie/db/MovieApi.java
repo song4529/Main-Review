4be618cba4b484771229e4b7ccb5b4658f8ee074
song4529 <gusdn4529@gmail.com>
2023-11-04 23:06:29 +0900

    feat : 영화 관련 api 가져오는 메서드 틀 작성

--- src/main/java/com/project/moviepop/movie/db/MovieApi.java
+++ src/main/java/com/project/moviepop/movie/db/MovieApi.java
@@ -1,4 +1,24 @@
 package com.project.moviepop.movie.db;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class MovieApi {
+    public static Set<String> getMovieList() {
+        return null;
+    }
 }
