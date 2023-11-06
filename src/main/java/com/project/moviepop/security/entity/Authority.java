ed6a6835f721b7a8bd5c02d236d5660a8ea3218c
song4529 <gusdn4529@gmail.com>
2023-11-06 23:40:36 +0900

    feat : Authority 엔티티 생성 및 매핑 작업

---
+++ src/main/java/com/project/moviepop/security/entity/Authority.java
@@ -0,0 +1,29 @@
+package com.project.moviepop.security.entity;
+
+import com.project.moviepop.user.entity.User;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+@Entity
+public class Authority implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authorityId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    private String role;
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+}
