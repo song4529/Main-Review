b3e56c82b86ac65f80e739a874b62e9b72c4524d
song4529 <gusdn4529@gmail.com>
2023-11-06 23:41:01 +0900

    feat : JwtAuthenticationFilter 틀 작성

---
+++ src/main/java/com/project/moviepop/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,10 @@
+package com.project.moviepop.security.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+}
