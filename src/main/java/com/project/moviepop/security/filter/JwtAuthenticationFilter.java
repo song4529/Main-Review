61971bb890a88a5ada062ff7880084160404e3aa
song4529 <gusdn4529@gmail.com>
2023-11-20 23:21:21 +0900

    feat : 내부 필터를 처리하는 메서드 틀 작성

--- src/main/java/com/project/moviepop/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/project/moviepop/security/filter/JwtAuthenticationFilter.java
@@ -7,10 +7,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenizer jwtTokenizer;
     private final CustomUserDetailService customUserDetailService;
     private final LogoutAccessTokenRedisRepository logoutAccessTokenRedisRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //나머지 작성
+    }
 }
