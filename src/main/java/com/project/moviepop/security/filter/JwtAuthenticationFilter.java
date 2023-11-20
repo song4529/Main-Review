050361e879abc154432c713dfe76295f33b53cef
song4529 <gusdn4529@gmail.com>
2023-11-20 23:17:57 +0900

    feat : JwtAuthenticationFilter 의존성 추가

--- src/main/java/com/project/moviepop/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/project/moviepop/security/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.project.moviepop.security.filter;
 
+import com.project.moviepop.security.jwt.JwtTokenizer;
+import com.project.moviepop.security.redis.repository.LogoutAccessTokenRedisRepository;
+import com.project.moviepop.security.userdetails.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -7,4 +10,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomUserDetailService customUserDetailService;
+    private final LogoutAccessTokenRedisRepository logoutAccessTokenRedisRepository;
 }
