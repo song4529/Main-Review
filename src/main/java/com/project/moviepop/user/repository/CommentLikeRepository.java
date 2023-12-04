bb83537268dcf9641d6ee522834029e4062e29a9
song4529 <gusdn4529@gmail.com>
2023-12-04 23:56:22 +0900

    feat : CommentLikeRepository 의존성 부여

--- src/main/java/com/project/moviepop/user/repository/CommentLikeRepository.java
+++ src/main/java/com/project/moviepop/user/repository/CommentLikeRepository.java
@@ -1,4 +1,7 @@
 package com.project.moviepop.user.repository;
 
-public interface CommentLikeRepository {
+import com.project.moviepop.user.entity.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 }
