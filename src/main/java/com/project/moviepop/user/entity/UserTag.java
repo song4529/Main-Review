b8fb7620aabc50524ac026694dc5c9bfbd90251c
song4529 <gusdn4529@gmail.com>
2023-12-08 23:26:06 +0900

    feat : UserTag 애너테이션 추가 및 필드 구성 요소 추가

--- src/main/java/com/project/moviepop/user/entity/UserTag.java
+++ src/main/java/com/project/moviepop/user/entity/UserTag.java
@@ -1,4 +1,17 @@
 package com.project.moviepop.user.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class UserTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userTagId;
 }
